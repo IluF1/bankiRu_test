4fc37e27935493a2a14f83a27d8059d82c5ffa58
IluF1 <pashadurov12@gmail.com>
2024-06-27 10:13:22 +0300

    fix optimization

--- src/utils/store/useFilterStore.tsx
+++ src/utils/store/useFilterStore.tsx
@@ -1,51 +1,49 @@
 // TODO:Можно сделать и без zustand
+//
 import { create } from 'zustand'
 import { IData, IFilterStoreState } from './helpers/interfaces'
 
 const URL = '../../../api/mock.json'
+const response = await fetch(URL, {
+	method: 'GET',
+})
+const data = await response.json()
+const products = data.products
 
 export const useFilterStore = create<IFilterStoreState>(set => ({
-  // Устанавлеваем начальные значения
-  amount: 0,
-  filter: '',
-  data: [],
+	// Устанавлеваем начальные значения
+	amount: 0,
+	filter: '',
+	data: [],
 
-  // Устанавливаем сумму
-  setAmount: (amount: number) => set(state => ({ ...state, amount })),
+	// Устанавливаем сумму
+	setAmount: (amount: number) => set(state => ({ ...state, amount })),
 
-  fetchData: async (amount: number, filter: string) => {
-    const response = await fetch(URL, {
-      method: 'GET'
-    })
-    const data = await response.json()
-    const products = data.products
+	fetchData: async (amount: number, filter: string) => {
+		let filteredData = products
 
-    let filteredData = products
+		// Проверям есть ли в инпуте сумма, если да то выводим кредиты которые не больше этой суммы
+		if (amount > 0) {
+			filteredData = products.filter(
+				(product: IData) => product.amount <= amount
+			)
+		}
 
-    // Проверям есть ли в инпуте сумма, если да то выводим кредиты которые не больше этой суммы
-    if (amount > 0) {
-      filteredData = products.filter(
-        (product: IData) => product.amount <= amount
-      )
-    }
+		// Сортируем массив в зависимости от выбранного фильтра
+		if (filter === 'По максимальной сумме') {
+			filteredData.sort((a: IData, b: IData) => b.amount - a.amount)
+		} else if (filter === 'По минимальной сумме') {
+			filteredData.sort((a: IData, b: IData) => a.amount - b.amount)
+		}
 
-    // Сортируем массив в зависимости от выбранного фильтра
-    if (filter === 'По максимальной сумме') {
-      filteredData.sort((a: IData, b: IData) => b.amount - a.amount)
-    } else if (filter === 'По минимальной сумме') {
-      filteredData.sort((a: IData, b: IData) => a.amount - b.amount)
-    }
+		// Обновляем состояние с отфильтрованными и отсортированными данными
+		set(state => ({
+			...state,
+			filter,
+			data: filteredData,
+		}))
+	},
 
-
-
-    // Обновляем состояние с отфильтрованными и отсортированными данными
-    set(state => ({
-      ...state,
-      filter,
-      data: filteredData,
-    }))
-  },
-
-  // Устанавливаем фильтр
-  setFilter: (filter: string) => set(state => ({ ...state, filter })),
+	// Устанавливаем фильтр
+	setFilter: (filter: string) => set(state => ({ ...state, filter })),
 }))
